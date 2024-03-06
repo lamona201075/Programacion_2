3e016f24688c1f0949806f5c9243d62d8817f1a4
lamona201075 <lamona201075@gmail.com>
2024-03-06 00:54:35 -0500

    test2

--- proyecto.js
+++ proyecto.js
@@ -3,6 +3,10 @@ let numaletas = 0;
 const tope = 10;
 
 while (numaletas < tope){
+    let origenMaleta = readlineSync.question(`Ingrese el origen de la maleta: `);
+
+    let numVuelo = +readlineSync.question(`Ingrese el numero del vuelo: `);
+    numVuelo = parseInt(numVuelo);
 
     let pesoMaleta = readlineSync.question(`Ingrese el peso de la maleta en KG: `);
     if(pesoMaleta.length == 0){
@@ -19,16 +23,19 @@ while (numaletas < tope){
             let costoAdicional = kilosExtra * costoExtra;
             console.log("El costo adicional por exceder el límite de peso es: " + costoAdicional + " pesos.");
         }
-        
-    
+
+        let genero = readlineSync.question(`Ingrese si el duenio de la maleta es hombre o mujer:  ` );
+        if (genero == `hombre`){
+            console.log(`El propietario de la maleta es hombre`);
+        }else if (genero == `mujer`){
+            console.log(`El duenio de la maleta es mujer. `);
+        }
     }
-    
-    /*let destino = readlineSync.question(`Ingrese el destino al que viaja: `);
-    let origenMaleta = readlineSync.question(`Ingrese el origen de la maleta: `);
+
+    /*
     el + al principio convierte en numero 
-    let numVuelo = +readlineSync.question(`Ingrese el numero del vuelo: `);
-    numVuelo = parseInt(numVuelo);
-    let generoDuemal = readlineSync.question(`Ingrese el genero del duenio de la maleta F/M: ` );
+    
+    let destino = readlineSync.question(`Ingrese el destino al que viaja: `);
 
     numaletas = numaletas + 1;*/
 
