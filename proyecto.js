75571f689249bd0f53f6badab623b80e4fd21ff4
lamona201075 <lamona201075@gmail.com>
2024-03-06 00:30:07 -0500

    prueba

--- proyecto.js
+++ proyecto.js
@@ -1,19 +1,39 @@
 const readlineSync = require('readline-sync');
-do 
-{
-    let origenMaleta = readlineSync.question(`Ingrese el origen de la maleta: `);
+let numaletas = 0;
+const tope = 10;
 
-    let numVuelo = readlineSync.question(`Ingrese el número del vuelo: `);
-    numVuelo = parseInt(numVuelo);
+while (numaletas < tope){
 
-    let destino = readlineSync.question(`Ingrese el destino al que viaja: `);
     let pesoMaleta = readlineSync.question(`Ingrese el peso de la maleta en KG: `);
-    let generoDuemal = readlineSync.question(`Ìngrese el género del dueño de la maleta F/M: ` );
-
-     // Lógica para determinar si el usuario desea continuar ingresando información
-     let sigMaleta = readlineSync.question(`¿Desea ingresar información para otra maleta? (s/n): `);
-     if (sigMaleta.toLowerCase() !== 's') {
-         continuar = false;
+    if(pesoMaleta.length == 0){
+        console.error(`El peso de la maleta no puede ser cero`);
+    }
+    else
+    {   
+        const limitePeso = 23;
+        const costoExtra = 20000;
+        const kilosExtra = pesoMaleta - limitePeso;
+        if(pesoMaleta < 23){
+            console.log(`No hay exceso de peso`);
+        }else{
+            let costoAdicional = kilosExtra * costoExtra;
+            console.log("El costo adicional por exceder el límite de peso es: " + costoAdicional + " pesos.");
+        }
+        
     
+    }
+    
+    /*let destino = readlineSync.question(`Ingrese el destino al que viaja: `);
+    let origenMaleta = readlineSync.question(`Ingrese el origen de la maleta: `);
+    el + al principio convierte en numero 
+    let numVuelo = +readlineSync.question(`Ingrese el numero del vuelo: `);
+    numVuelo = parseInt(numVuelo);
+    let generoDuemal = readlineSync.question(`Ingrese el genero del duenio de la maleta F/M: ` );
+
+    numaletas = numaletas + 1;*/
 
 }
+
+
+    
+
